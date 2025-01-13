76f810128ce8761e1c20ee0db69b023483e86125
Beksultan Chomoev <chomoevbeksultan@gmail.com>
2025-01-12 20:53:41 -0600

    User sessions route is done.

--- api/routers/users.ts
+++ api/routers/users.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import User from "../models/User";
 import {Error} from "mongoose";
+import bcrypt from "bcrypt";
 
 const usersRouter = express.Router();
 
@@ -24,7 +25,32 @@ usersRouter.post('/', async (req, res, next) => {
 });
 
 usersRouter.post('/sessions', async (req, res, next) => {
-    res.send('sessions');
+    try {
+        const user = await User.findOne({username: req.body.username});
+
+        if (!user) {
+            res.status(400).send({error: 'Username not found'});
+            return;
+        }
+
+        const isMatch = await bcrypt.compare(req.body.password, user.password);
+
+        if (!isMatch) {
+            res.status(400).send({error: 'Password is wrong'});
+            return;
+        }
+
+        user.generateToken();
+        await user.save();
+        res.send({message: 'Username and password are correct', user});
+        return;
+    } catch (error) {
+        if (error instanceof Error.ValidationError) {
+            res.status(400).send(error);
+            return;
+        }
+        next(error);
+    }
 });
 
 export default usersRouter;
