d8be93a53335e937b12087a7653b6d8ba876ca4e
Beksultan Chomoev <chomoevbeksultan@gmail.com>
2025-01-13 01:21:21 -0600

    Deleted cors and public static functions from index. HW-84 is done.

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import config from "./config";
 import * as mongoose from "mongoose";
-import cors from "cors";
 import mongoDb from "./mongoDb";
 import usersRouter from "./routers/users";
 import tasksRouter from "./routers/tasks";
@@ -9,9 +8,7 @@ import tasksRouter from "./routers/tasks";
 const app = express();
 const port = 8000;
 
-app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
 
 app.use('/users', usersRouter);
 app.use('/tasks', tasksRouter);
